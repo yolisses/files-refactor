4e6e52cd5642103cddfe2b9b8592f9a278c7e8b2
yolisses <iamurise@gmail.com>
2024-05-15 12:33:53 -0300

    implement double link imports

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,19 @@ class Folder {
 
 // TODO find a better name
 class FileNode {
-  constructor(
-    public name: string,
-    public folder: Folder,
-    public importedBy: FileNode[]
-  ) {}
+  imports: FileNode[] = [];
+  importedBy: FileNode[] = [];
+
+  constructor(public name: string, public folder: Folder) {}
+
+  addImport(file: FileNode) {
+    this.imports.push(file);
+    file.importedBy.push(this);
+  }
 }
 
-function getGraphvizGraph(files: FileNode[]) {
-  const g = graphviz.digraph("G");
+function createFolderClusters(files: FileNode[], g: Graph) {
   const clusters: Record<string, Graph> = {};
-
   files.forEach((file) => {
     const { folder } = file;
     if (!clusters[folder.name]) {
@@ -28,37 +30,39 @@ function getGraphvizGraph(files: FileNode[]) {
       clusters[folder.name] = folderCluster;
     }
   });
+  return clusters;
+}
+
+function getGraphvizGraph(files: FileNode[]) {
+  const g = graphviz.digraph("G");
+  const folderClusters = createFolderClusters(files, g);
 
   files.forEach((file) => {
-    // const cluster = clusters[file.folder.name];
-    // cluster.set("label", file.folder.name);
+    const folderCluster = folderClusters[file.folder.name];
 
-    const cluster = g;
-
-    const fileNode = cluster.addNode(file.name);
+    const fileNode = folderCluster.addNode(file.name);
     fileNode.set("label", `${file.name}\n${file.folder.name}`);
 
-    if (file.folder.name === "unused_files") {
-      fileNode.set("style", "filled");
-      fileNode.set("fillcolor", "gray");
-    }
-
-    file.importedBy.forEach((importer) => {
-      g.addEdge(importer.name, file.name);
+    file.imports.forEach((importedFile) => {
+      g.addEdge(file.name, importedFile.name);
     });
   });
   return g;
 }
 
-function createRandomFilesTree(count: number) {
+function createRandomFiles(count: number) {
   const rootFolder = new Folder("root");
+
   const files = times(count).map((i) => {
-    return new FileNode(`${i}`, rootFolder, []);
+    return new FileNode(`${i}`, rootFolder);
   });
 
   files.forEach((file) => {
-    const randomNumberOfImports = randomInteger(0, 2);
-    file.importedBy = randomSample(files, randomNumberOfImports);
+    const importsCount = randomInteger(0, 2);
+    const imports = randomSample(files, importsCount);
+    imports.forEach((importedFile) => {
+      file.addImport(importedFile);
+    });
   });
 
   return files;
@@ -71,14 +75,24 @@ function organizeFiles(files: FileNode[]) {
   });
 
   files.forEach((file) => {
-    if (file.importedBy.length === 1) {
+    if (file.importedBy.length === 1 && file.imports.length === 0) {
       const importer = file.importedBy[0];
       file.folder = importer.folder;
     }
   });
 }
 
-const randomFiles = createRandomFilesTree(20);
+const randomFiles = createRandomFiles(5);
+
+console.log(
+  randomFiles.map((file) => {
+    return {
+      name: file.name,
+      imports: file.imports.map((importedFile) => importedFile.name),
+      importedBy: file.importedBy.map((importer) => importer.name),
+    };
+  })
+);
 organizeFiles(randomFiles);
 
 const graphvizGraph = getGraphvizGraph(randomFiles);
