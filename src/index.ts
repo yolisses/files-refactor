5bcb05224f3242d3c570002d22f07e790ebd5084
yolisses <iamurise@gmail.com>
2024-05-19 10:31:36 -0300

    plot folder

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,50 @@
 import { digraph } from "graphviz";
 
+class Folder {
+  files: FileNode[] = [];
+  constructor(public name: string) {}
+
+  addFile(file: FileNode) {
+    this.files.push(file);
+  }
+}
+
 // TODO find a better name
 class FileNode {
   constructor(public name: string, public imports: FileNode[]) {}
 }
 
-const file1 = new FileNode("file1", []);
-const file2 = new FileNode("file2", [file1]);
-const file3 = new FileNode("file3", [file1, file2]);
+const root = new Folder("root");
+const folders = [root];
 
+const file1 = new FileNode("1", []);
+const file2 = new FileNode("2", [file1]);
+const file3 = new FileNode("3", [file1, file2]);
 const files = [file1, file2, file3];
 
-function createGraphVisualization(files: FileNode[]) {
+root.addFile(file1);
+root.addFile(file2);
+root.addFile(file3);
+
+function createGraphVisualization() {
   const g = digraph("G");
 
+  folders.forEach((folder) => {
+    const cluster = g.addCluster("cluster_" + folder.name);
+    cluster.set("label", folder.name);
+    folder.files.forEach((file) => {
+      cluster.addNode(file.name);
+    });
+  });
+
   files.forEach((file) => {
-    g.addNode(file.name);
     file.imports.forEach((importedFile) => {
       g.addEdge(file.name, importedFile.name);
     });
   });
+
   return g;
 }
 
-const graphVisualization = createGraphVisualization(files);
+const graphVisualization = createGraphVisualization();
 graphVisualization.output("svg", "output.svg");
