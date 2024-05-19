833db1f42592488eff0c192eef9c78103669cc77
yolisses <iamurise@gmail.com>
2024-05-19 10:54:38 -0300

    create inode

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,70 @@
 import { Graph, digraph } from "graphviz";
 
-class Folder {
-  files: FileNode[] = [];
-  subfolders: Folder[] = [];
+class INode {
   parent: Folder | null = null;
   constructor(public name: string) {}
 
-  addFile(file: FileNode) {
-    this.files.push(file);
+  move(newParent: Folder) {
+    if (this.parent) {
+      this.parent.removeFile(this);
+    }
+    newParent.addChild(this);
+  }
+}
+
+class Folder extends INode {
+  children: INode[] = [];
+
+  addChild(inode: INode) {
+    this.children.push(inode);
   }
 
-  addFolder(folder: Folder) {
-    folder.parent = this;
-    this.subfolders.push(folder);
+  removeFile(inode: INode) {
+    const index = this.children.indexOf(inode);
+    if (index !== -1) {
+      this.children.splice(index, 1);
+    }
+  }
+
+  get files() {
+    return this.children.filter(
+      (child) => child instanceof FileNode
+    ) as FileNode[];
+  }
+
+  get subfolders() {
+    return this.children.filter((child) => child instanceof Folder) as Folder[];
   }
 }
 
 // TODO find a better name
-class FileNode {
-  constructor(public name: string, public imports: FileNode[]) {}
+class FileNode extends INode {
+  imports: FileNode[] = [];
+
+  addImport(file: FileNode) {
+    this.imports.push(file);
+  }
 }
 
 const root = new Folder("root");
 const folder1 = new Folder("folder1");
 const folder2 = new Folder("folder2");
 
-root.addFolder(folder1);
-root.addFolder(folder2);
+root.addChild(folder1);
+root.addChild(folder2);
 
-const file1 = new FileNode("1", []);
-const file2 = new FileNode("2", [file1]);
-const file3 = new FileNode("3", [file1, file2]);
+const file1 = new FileNode("1");
+const file2 = new FileNode("2");
+const file3 = new FileNode("3");
 const files = [file1, file2, file3];
 
-folder1.addFile(file1);
-folder2.addFile(file2);
-root.addFile(file3);
+file1.addImport(file2);
+file1.addImport(file3);
+file2.addImport(file3);
+
+folder1.addChild(file1);
+folder2.addChild(file2);
+root.addChild(file3);
 
 function plotFolders(g: Graph, folder: Folder) {
   const cluster = g.addCluster("cluster_" + folder.name);
